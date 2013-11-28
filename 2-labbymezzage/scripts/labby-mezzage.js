cda7796dafd6b9f1dbc835be23b12f745176a409
Hannes <hl222ih@student.lnu.se>
2013-11-28 17:40:52 +0100

    labbymezzage, (mer moment 2, +JSDoc av message.js, html fix, css fix)

--- 2-labbymezzage/scripts/labby-mezzage.js
+++ 2-labbymezzage/scripts/labby-mezzage.js
@@ -1,19 +1,22 @@
 /*global Message*/
 
+var messageBoard;
+
 function displayMessage(message) {
     "use strict";
     window.alert(message);
 }
 
-function init() {
+function messageBoard() {
     "use strict";
     var inputArea = document.getElementById("input"),
         textArea = inputArea.getElementsByTagName("textarea")[0],
         sendButton = document.getElementById("send"),
         message = new Message("text", new Date());
 
-
-    sendButton.onclick = function () { displayMessage(message.getFullDateText()); };
+    sendButton.onclick = function () {
+        displayMessage(message.getDateTimeText());
+    };
 }
 
-window.onload = init;
+window.onload = messageBoard;
--- 2-labbymezzage/scripts/message.js
+++ 2-labbymezzage/scripts/message.js
@@ -1,42 +1,77 @@
+/**
+ * @constructor Ett meddelande.
+ * @param {string} text Meddelandets text.
+ * @param {Date} date Meddelandets datum.
+ */
 function Message(text, date) {
     "use strict";
+    //endast åtkomst via getters och setters.
+    var textMessage,      //meddelandets text
+        dateOfMessage;    //meddelandets datum-objekt
+
     this.getText = function () {
-        return text;
+        return textMessage;
     };
     this.setText = function (text) {
-        this.text = text;
+        textMessage = text;
     };
     this.getDate = function () {
-        return date;
+        return dateOfMessage;
     };
     this.setDate = function (date) {
-        this.date = date;
+        dateOfMessage = date;
     };
 }
 
+/**
+ * "Privat" hjälpfunktion för att skapa korrekt tid- och datum format med inledande nolla.
+ * @param number {number|string}
+ * @returns {string} Om det inskickade argumentet är ett tecken långt, det inskickade argumentet med "0" framför, annars, det inskickade argumentet.
+ */
 var padZero = function (number) {
     "use strict";
     window.console.log(number.length);
     return (number.toString().length === 1) ? "0" + number : number.toString();
 };
 
+
+/**
+ * Överlagring av default-toString().
+ * @returns {string} Meddelandets text och datum.
+ */
 Message.prototype.toString = function () {
     "use strict";
     return this.getText() + " (" + this.getDate() + ")";
 };
+
+/**
+ * @returns {string} Meddelandets text i html-format.
+ */
 Message.prototype.getHtmlText = function () {
     "use strict";
     return this.getText().replace(/\n/g, '<br />');
 };
+
+/**
+ * @returns {string} Meddelandets datum i formatet YYYY-MM-DD.
+ */
 Message.prototype.getDateText = function () {
     "use strict";
     return this.getDate().getFullYear() + "-" + padZero((this.getDate().getMonth() + 1)) + "-" + padZero(this.getDate().getDate());
 };
+
+/**
+ * @returns {string} Meddelandets tid i formatet HH:MM:SS.
+ */
 Message.prototype.getTimeText = function () {
     "use strict";
     return padZero(this.getDate().getHours()) + ":" + padZero(this.getDate().getMinutes()) + ":" + padZero(this.getDate().getSeconds());
 };
-Message.prototype.getFullDateText = function () {
+
+/**
+ * @returns {string} Meddelandets datum och tid i formatet YYYY-MM-DD HH:MM:SS.
+ */
+Message.prototype.getDateTimeText = function () {
     "use strict";
     return this.getDateText() + " " + this.getTimeText();
 };
