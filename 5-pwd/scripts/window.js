7cea7ea662300aadd8b2671170620d8fdd9d2b6e
Hannes <hl222ih@student.lnu.se>
2014-01-04 16:48:08 +0100

    projekt stängknapp till fönstret som stänger fönstret om man klickar på det

--- 5-pwd/scripts/window.js
+++ 5-pwd/scripts/window.js
@@ -5,16 +5,36 @@ var NS1DV403 = NS1DV403 || {};
 NS1DV403.Window = function (height, width, name, hasMenuBar, hasStatusBar) {
     var appWindow,
         appTitleBar,
+        appRemoveButtonAnchor,
+        appRemoveButton,
         appMenuBar,
         appContent,
-        appStatusBar;
+        appStatusBar,
+        appP;
 
     appWindow = document.createElement('div');
     appWindow.setAttribute('class', 'appWindow');
     appWindow.setAttribute('style', 'height: ' + height + "px; width: " + width + "px;");
     appTitleBar = document.createElement('div');
     appTitleBar.setAttribute('class', 'appTitleBar');
-    appTitleBar.appendChild(document.createTextNode(name));
+    appP = document.createElement('p');
+    appP.setAttribute('class', 'appTitleBarText');
+    appP.appendChild(document.createTextNode(name));
+    appTitleBar.appendChild(appP);
+    appRemoveButtonAnchor = document.createElement('a');
+    appRemoveButtonAnchor.setAttribute('class', 'appRemoveButtonAnchor');
+    appRemoveButtonAnchor.setAttribute('href', '#');
+    appRemoveButtonAnchor.setAttribute('alt', 'stäng fönster');
+
+    appRemoveButtonAnchor.addEventListener('click', function (e) {
+        e.target.parentNode.parentNode.parentNode.parentNode.removeChild(appWindow);
+    });
+
+    appRemoveButton = document.createElement('img');
+    appRemoveButton.setAttribute('class', 'appRemoveButton');
+    appRemoveButton.setAttribute('src', 'images/remove.png');
+    appRemoveButtonAnchor.appendChild(appRemoveButton);
+    appTitleBar.appendChild(appRemoveButtonAnchor);
     appWindow.appendChild(appTitleBar);
 
     if (hasMenuBar) {
