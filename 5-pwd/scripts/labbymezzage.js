9e5e392e5cfecebfd4eccd46808c65b56aff3970
Hannes <hl222ih@student.lnu.se>
2014-01-13 02:23:00 +0100

    projekt börjar på labbymezzage, flyttar över kod från labbuppgiften

--- 5-pwd/scripts/labbymezzage.js
+++ 5-pwd/scripts/labbymezzage.js
@@ -12,3 +12,145 @@ NS1DV403.LabbyMezzage = function (height, width) {
     };
 
 };
+
+
+/*global Message*/
+
+/**
+ * @constructor En meddelandehanterare.
+ */
+function MessageBoard() {
+    "use strict";
+    var sendButton,
+        textArea;
+
+    this.messages = [];
+    this.render();
+}
+
+/**
+ * Skapar och skriver ut html-koden för MessageBoard.
+ */
+MessageBoard.prototype.render = function () {
+    "use strict";
+    var container = document.getElementById("container"),//html-dokumentets container-node.
+        messageBoardNode = document.createElement("div"),//skapar ett node-träd för en ny meddelandehanterare.
+        content = document.createElement("div"),         //...
+        input = document.createElement("div"),
+        textArea = document.createElement("textarea"),
+        sendButton = document.createElement("button"),
+        existingMessageBoardNodes = document.getElementsByClassName("messageBoard"), //en array med alla meddelarehanterare-noder.
+        count,          //för användning i loopar.
+        that = this,    //för användning av yttre variabler i closure
+        tempAttribute,  //för att kunna göra ändringar i elementets attribut med hänsyn till den information som redan finns i elementets attribut.
+        footer,         //html-dokumentets footer-node.
+        send;           //funktion för att "skicka meddelande", skapar meddelandet, lägger till händelsehanterare, skriver ut det, med mera.
+
+    for (count = 0; count < existingMessageBoardNodes.length; count++) {
+        tempAttribute = existingMessageBoardNodes[count].getAttribute("class");
+        if (!/invisible/.test(tempAttribute)) {
+            existingMessageBoardNodes[count].setAttribute("class", tempAttribute + " invisible");
+        }
+    }
+
+    //skapar attribut för de skapade elementen
+    messageBoardNode.setAttribute("class", "messageBoard");
+    content.setAttribute("class", "content");
+    input.setAttribute("class", "input");
+    textArea.setAttribute("autofocus", "autofocus");
+    textArea.setAttribute("wrap", "soft");
+    textArea.setAttribute("placeholder", "Skriv någonting och tryck på ENTER för att skicka...");
+    sendButton.setAttribute("class", "send");
+    sendButton.innerHTML = "SKICKA";
+
+    //kopplar ihop de skapade elementen.
+    input.appendChild(textArea);
+    input.appendChild(sendButton);
+    messageBoardNode.appendChild(content);
+    messageBoardNode.appendChild(input);
+    footer = container.lastElementChild;
+    container.removeChild(footer);
+    container.appendChild(messageBoardNode);
+    container.appendChild(footer);
+
+    /**
+     * Funktion för att skapa ett meddelande och sedan skriva ut det. Anropas av händelsehanterare.
+     * @param e Händelsen som triggade.
+     */
+    send = function (e) {
+        var message,
+            buttons,
+            removeButton,
+            timeButton;
+
+        if (textArea.value.trim() !== "") {
+            message = new Message(textArea.value, new Date());
+
+            that.messages.push(message);
+            document.getElementById("numberOfMessages").innerHTML = that.messages.length.toString();
+            buttons = message.render(e); //skriver ut det nyligen skapade meddelandet i html-dokumentet. Fångar knapparna i dessa meddelanden så att händelsehanterare kan kopplas till dem.
+            timeButton = buttons[0];
+            removeButton = buttons[1];
+
+            /**
+             * Händelsehanterare för klick det nyligen skapade meddelandets radera-knapp.
+             */
+            removeButton.addEventListener('click', function (e) {
+                var nodeToRemove = e.target.parentNode.parentNode,    //noden som ska tas bort
+                    parentNodeToRemoveFrom = nodeToRemove.parentNode, //föräldern till noden som ska tas bort
+                    index = 0,                                        //för att räkna ut meddelandets position
+                    node = nodeToRemove;                              //för att räkna ut meddelandets position
+
+                if (window.confirm("Vill du verkligen radera meddelandet?")) { //kontrollerar om användaren verkligen vill radera meddelandet. Annars görs ingenting.
+                    while (node.previousElementSibling !== null) {
+                        node = node.previousElementSibling;
+                        index++;  //räknar ut meddelande-nodens position.
+                    }
+                    parentNodeToRemoveFrom.removeChild(nodeToRemove); //tar bort meddelande-noden från html-dokumentet.
+                    that.messages.splice(index, 1);  //tar bort meddelandet från arrayen på angiven position.
+                    document.getElementById("numberOfMessages").innerHTML = that.messages.length.toString();
+                    textArea.focus();
+                }
+            }, false);
+
+            /**
+             * Händelsehanterare för det nyligen skapade meddelandets tid-knapp.
+             */
+            timeButton.addEventListener('click', function (e) {
+                var node = e.target.parentNode.parentNode,     //den meddelande-node som innehåller den aktuella tid-knappen.
+                    index = 0;                                 //index för att hålla reda på meddelandenoden.
+
+                while (node.previousElementSibling !== null) { //räknar ut meddelandets position i den aktuella meddelandehanteraren.
+                    node = node.previousElementSibling;
+                    index++;
+                }
+
+                window.alert("Meddelandet skickades " + that.messages[index].getDateTimeText() + ".");
+
+            }, false);
+
+            textArea.value = ""; //tömmer textarea-elementet.
+        }
+
+        textArea.focus(); //sätter fokus till textarea-elementet.
+
+    };
+
+    /**
+     * Händelsehanterare för när användaren trycker på <Enter> efter att ha skrivit ett meddelande i textarea-elementet.
+     */
+    textArea.addEventListener('keydown', function (e) {
+        if (e.keyCode === 13 && e.shiftKey !== true) {
+            send(e);
+            e.preventDefault(); //så inte en radbrytning läggs till i meddelanderutan efter det att meddelandet skickats.
+        }
+    }, false);
+
+    /**
+     * Händelsehanterare för när användaren klickar på skicka-knappen intill textarea-elementet.
+     */
+    sendButton.addEventListener('click', function (e) {
+        send(e);
+    }, false);
+
+};
---
+++ 5-pwd/scripts/message.js
@@ -0,0 +1,127 @@
+/*...*/
+
+/**
+ * @constructor Ett meddelande.
+ * @param {string} text Meddelandets text.
+ * @param {Date} date Meddelandets datum.
+ */
+function Message(text, date) {
+    "use strict";
+    this.textMessage = text;
+    this.dateOfMessage = date;
+
+    this.getText = function () {
+        return text;
+    };
+
+    this.setText = function (value) {
+        text = value;
+    };
+
+    this.getDate = function () {
+        return date;
+    };
+
+    this.setDate = function (value) {
+        date = value;
+    };
+}
+
+/**
+ * "Privat" hjälpfunktion för att skapa korrekt tid- och datum format med inledande nolla.
+ * @param number {number|string}
+ * @returns {string} Om det inskickade argumentet är ett tecken långt, det inskickade argumentet med "0" framför, annars, det inskickade argumentet.
+ */
+var padZero = function (number) {
+    "use strict";
+    return (number.toString().length === 1) ? "0" + number : number.toString();
+};
+
+
+/**
+ * Överlagring av default-toString().
+ * @returns {string} Meddelandets text och datum.
+ */
+Message.prototype.toString = function () {
+    "use strict";
+    return this.getText() + " (" + this.getDate() + ")";
+};
+
+/**
+ * @returns {string} Meddelandets text i html-format.
+ */
+Message.prototype.getHtmlText = function () {
+    "use strict";
+    return this.getText().replace(/[\n\r]/g, '<br />');
+};
+
+/**
+ * @returns {string} Meddelandets datum i formatet YYYY-MM-DD.
+ */
+Message.prototype.getDateText = function () {
+    "use strict";
+    return this.getDate().getFullYear() + "-" + padZero((this.getDate().getMonth() + 1)) + "-" + padZero(this.getDate().getDate());
+};
+
+/**
+ * @returns {string} Meddelandets tid i formatet HH:MM:SS.
+ */
+Message.prototype.getTimeText = function () {
+    "use strict";
+    return padZero(this.getDate().getHours()) + ":" + padZero(this.getDate().getMinutes()) + ":" + padZero(this.getDate().getSeconds());
+};
+
+/**
+ * @returns {string} Meddelandets datum och tid i formatet YYYY-MM-DD HH:MM:SS.
+ */
+Message.prototype.getDateTimeText = function () {
+    "use strict";
+    return this.getDateText() + " " + this.getTimeText();
+};
+
+/**
+ * Skapar och skriver ut html-koden för Message. Returnerar en array med node-objekten timeButton och removeButton
+ * som behöver kopplas händelsehanterare till.
+ * @returns {Array}
+ */
+Message.prototype.render = function (e) {
+    "use strict";
+    var createHere = e.target.parentNode.previousElementSibling, //platsen där html-elementet ska skapas.
+        message = document.createElement("section"),             //skapar message-elementet.
+        buttons = document.createElement("div"),                 //...
+        timeImg = document.createElement("img"),
+        timeButton = document.createElement("a"),
+        removeImg = document.createElement("img"),
+        removeButton = document.createElement("a"),
+        text = document.createElement("p"),
+        dateTime = document.createElement("div");
+
+    message.setAttribute("class", "message");
+    buttons.setAttribute("class", "buttons");
+    timeImg.setAttribute("src", "images/time.png");
+    timeButton.setAttribute("class", "timeButton");
+    timeButton.setAttribute("href", "#");
+    removeImg.setAttribute("src", "images/remove.png");
+    removeButton.setAttribute("class", "removeButton");
+    removeButton.setAttribute("href", "#");
+    text.innerHTML = this.getHtmlText();
+    dateTime.setAttribute("class", "dateTime");
+    dateTime.innerHTML = this.getTimeText();
+
+    message.appendChild(buttons);
+    buttons.appendChild(timeButton);
+    timeButton.appendChild(timeImg);
+    buttons.appendChild(removeButton);
+    removeButton.appendChild(removeImg);
+    message.appendChild(text);
+    message.appendChild(dateTime);
+    createHere.appendChild(message);
+
+    createHere.scrollTop = createHere.scrollHeight; //så att scrollen flyttas längst ned så att det nyligen tillagda meddelandet syns.
+
+    return [timeButton, removeButton];
+
+};
+
+
+
